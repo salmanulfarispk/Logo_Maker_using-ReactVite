abd1f57a0622e4877fc32711ccdd809d7829d714
Salmanul FarisPK <salmanulfarispk1405@gmail.com>
2024-05-12 17:05:47 +0530

    first commit and project setups completes

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Button } from './components/ui/button'
+
+
+function App() {
+ 
+
+  return (
+    <>
+      
+        <h1>Hello</h1>
+        <h1 className="text-3xl font-bold underline text-red-600">
+      Hello world!
+    </h1>
+    <Button>Click me</Button>
+        
+    </>
+  )
+}
+
+export default App
